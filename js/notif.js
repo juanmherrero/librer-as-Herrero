c6260796966d23067f965c5ff3fe3a2ed2f53e48
juanmherrero <105325161+juanmherrero@users.noreply.github.com>
2022-08-03 19:37:02 -0300

    primera carga

---
+++ js/notif.js
@@ -0,0 +1,99 @@
+let btn = document.getElementById("agregar1");
+btn.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
+
+
+let btn2 = document.getElementById("agregar2");
+btn2.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
+
+let btn3 = document.getElementById("agregar3");
+btn3.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
+
+let btn4 = document.getElementById("agregar4");
+btn4.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
+
+let btn5 = document.getElementById("agregar5");
+btn5.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
+
+let btn6 = document.getElementById("agregar6");
+btn6.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
+
+let btn7 = document.getElementById("agregar7");
+btn7.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
+
+let btn8 = document.getElementById("agregar8");
+btn8.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
+
+let btn9 = document.getElementById("agregar9");
+btn9.addEventListener("click", () => {
+    Toastify({
+        text: "Producto añadido al carrito",
+        duration: 3000,
+        style: {
+        background: "linear-gradient(to left, #0081A7, #5ab9d6)",
+        },
+    }).showToast();
+});
